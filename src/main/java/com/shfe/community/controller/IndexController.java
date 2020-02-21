1dfe79b5510cc49a3960e9a2e4da4361ee1ffa84
richardzhi <richardzhi@gmail.com>
2020-02-21 22:59:08 +0800

    create page style

--- src/main/java/com/shfe/community/controller/IndexController.java
+++ src/main/java/com/shfe/community/controller/IndexController.java
@@ -18,22 +18,22 @@ public class IndexController {
     private UserMapper userMapper;
 
     @GetMapping("/")
-    public String hello(HttpServletRequest request){
+    public String index(HttpServletRequest request){
 //        读取请求带来的cookie信息，取出其中的token, 到后台数据库中查询token, 验证用户是否已登录
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if(cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user= userMapper.findByToken(token);
+        if (cookies != null ) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
 //                如果通过token 查找到用户，则设置session 的user属性未user对象，这样前端页面可以获得session对象来获取user对象，判断是否要展示登录还是用户名
-                if (user != null){
-                    request.getSession().setAttribute("user",user);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
                 }
-                break;
             }
         }
-
-
         return ("index");
 
     }
